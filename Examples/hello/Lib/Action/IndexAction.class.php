88689f1b507324d7d07d7fe7a8e6083782b106b9
taballa.hp-PD <taballa@gmail.com>
2013-04-12 21:15:25 +0800

    del Examples

--- Examples/hello/Lib/Action/IndexAction.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-class IndexAction extends Action{
-    public function index($name='ThinkPHP') {
-        $this->hello    =   'Hello,'.$name.'！';
-        $this->display();
-    }
-}
--- Examples/theme/Conf/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-    'DEFAULT_THEME'  	=> 	'default',
-    'THEME_LIST'		=>	'default,think',
-    'TMPL_DETECT_THEME' => 	true, // 自动侦测模板主题
-);
